e941dae8e049a6d176ac4f750020d304bce32566
P87585 <masipunk.anam@gmail.com>
2017-02-23 17:50:36 +0700

    auth

--- src/main/java/id/co/pegadaian/auth/AuthServerConfig.java
+++ src/main/java/id/co/pegadaian/auth/AuthServerConfig.java
@@ -21,15 +21,15 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
 	
-	 @Autowired
-     @Qualifier("authenticationManagerBean")
-     private AuthenticationManager authenticationManager;
-     
-     @Autowired
-     @Qualifier("userDetailsServiceBean")
-     private UserDetailsService userDetailsService;
-	 
-	@Override
+	@Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+    
+    @Autowired
+    @Qualifier("userDetailsServiceBean")
+    private UserDetailsService userDetailsService;
+    
+    @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("aplikasiweb")
@@ -37,22 +37,27 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
                 .secret("aplikasi123")
                 .authorizedGrantTypes("authorization_code", "refresh_token")
                 .scopes("modul-gadai", "modul-harga")
-        		.accessTokenValiditySeconds(180);
+//                .accessTokenValiditySeconds(180)
+                .and()
+                .withClient("aplikasijs")
+                .authorities("APLIKASI_CLIENT_OAUTH2")
+                .secret("aplikasi123")
+                .authorizedGrantTypes("password")
+                .scopes("modul-gadai", "modul-harga")
+                .autoApprove(true);
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security
-        	.checkTokenAccess("hasAuthority('APLIKASI_CLIENT_OAUTH2')")
-        	.tokenKeyAccess("permitAll()");
+        security.checkTokenAccess("hasAuthority('APLIKASI_CLIENT_OAUTH2')")
+                .tokenKeyAccess("permitAll()");
     }
-    
-    
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-        	.accessTokenConverter(jwtTokenConverter())
-        	.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+                .accessTokenConverter(jwtTokenConverter())
+                .authenticationManager(authenticationManager).userDetailsService(userDetailsService);
     }
     
     @Bean
--- src/main/java/id/co/pegadaian/auth/SecurityConfig.java
+++ src/main/java/id/co/pegadaian/auth/SecurityConfig.java
@@ -1,5 +1,8 @@
 package id.co.pegadaian.auth;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,15 +10,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	@Override
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String SQL_LOGIN = "select username, password, true as enabled "
+            + "from s_user where username= ? ";
+    private static final String SQL_PERMISSION = "select u.username, p.name "
+            + "from s_user u "
+            + "inner join s_user_role ur on u.id = ur.id_user "
+            + "inner join s_role r on r.id = ur.id_role "
+            + "inner join s_role_permission rp on rp.id_role = r.id "
+            + "inner join s_permission p on p.id = rp.id_permission "
+            + "where u.username = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .authorities("USER_VIEW")
-                .password("123");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION);
     }
 
     @Override
@@ -27,10 +47,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .and().authorizeRequests().anyRequest().authenticated();
     }
     
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
-       return super.authenticationManagerBean();
+        return super.authenticationManagerBean();
     }
     
     @Override
@@ -39,4 +64,3 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         return super.userDetailsServiceBean();
     }
 }
-
